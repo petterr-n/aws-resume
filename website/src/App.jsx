0a48c05fd5ee9afef1706f02d67da5db9a4660a0
pro028 <pro028@uit.no>
2025-10-09 14:15:43 +0200

    mhm

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from 'react'
 import Resume from './Resume'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Resume />
--- website/src/VisitorCounter.jsx
+++ website/src/VisitorCounter.jsx
@@ -22,7 +22,7 @@ export default function VisitorCounter() {
 
   return (
     <p id="visitorCounter">
-      Number of visitors: <span id="visitors-total">{visitorCount}</span>
+      Visitors: <span id="visitors-total">{visitorCount}</span>
     </p>
   );
 }
